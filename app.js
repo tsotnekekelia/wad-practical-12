e13d247207bd2c86621f0b9efc0c042a58706f99
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2019-11-18 15:37:31 +0200

    Added CORS

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 const usersRouter = require('./routes/users');
 
 const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
