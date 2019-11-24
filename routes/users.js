2eafcc619852ee0b7b28234a64f85810fb7c5ec0
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2019-11-24 17:30:26 +0200

    Working version

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,25 @@ router.get('/1/tasks', function (req, res, next) {
 
 });
 
+router.post('/1/tasks', function (req, res, next) {
+
+   let title = req.body.title;
+    fs.readFile('./data/tasks.json', (err, data) => {
+        if (err) throw err;
+        let tasks = JSON.parse(data);
+
+        let item = {
+            id: tasks.length + 1,
+            title: title,
+            done: false
+        };
+        tasks.push(item);
+
+        fs.writeFile('./data/tasks.json', JSON.stringify(tasks), (err) => {
+            if (err) throw err;
+            res.json(item);
+        })
+    });
+});
+
 module.exports = router;
